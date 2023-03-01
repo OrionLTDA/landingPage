9a451772de1dcb17e4ccf6456be0b697eaf621ae
unknown <gldhperes@gmail.com>
2023-03-01 16:25:26 -0300

    O MELHOR HEADER Q VOCE JA VIU NA VIDA

---
+++ js/menuBarController.js
@@ -0,0 +1,56 @@
+const menuBtn = $("#menuBtn")
+const menu =  $('.navbar ul')
+const rocket = $('#rocket')
+const rocketIcon = $('#rocket ion-icon')
+const impulse = $('#impulse')
+
+var menuOpen = false
+
+menuBtn.on('click', () => {
+    
+
+    if (!menuOpen){
+
+        menu.css({
+            'transform': 'translateX(0)'
+        });
+
+        rocket.css({
+            'transform': 'translateX(0)'
+        });
+
+        rocketIcon.css({
+            'transition': '.5 ease-in-out',
+            'transform': 'rotateZ(45deg)',
+        })
+
+        impulse.css({
+            'transition': '.5 ease-in-out',
+            'transform': 'rotateZ(180deg)',
+            'left': '5px',
+        })
+        
+    }else{
+        menu.css({
+            'transform': 'translateX(-350px)'
+        });
+
+        rocket.css({
+            'transform': 'translateX(-350px)'
+        });
+        
+        rocketIcon.css({
+            'transition': '.5 ease-in-out',
+            'transform': 'rotateZ(-135deg)',
+        })
+
+        impulse.css({
+            'transition': '.5 ease-in-out',
+            'transform': 'rotateZ(-180deg)',
+            'left': '32px',
+        })
+    }
+
+    menuOpen = !menuOpen
+    
+})
